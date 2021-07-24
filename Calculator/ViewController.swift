d3c2771d21e4f2d2d6e44bd7dce008b9c26fd3af
Anatolii Afarinov <87117971+Aafarinov@users.noreply.github.com>
2021-07-24 23:26:44 +0300

    before main function

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberFromScreen: Double = 0;
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        result.text = result.text! + String(sender.tag)
+        numberFromScreen = Double(result.text!)!
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text! != ""{
+            if sender.tag == 11 {// Деление
+                
+            }
+            else if sender.tag == 12 {// умножение
+        }
+            else if sender.tag == 13 {// вычитание
+            }
+            else if sender.tag == 14 {// Добавление
+            
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
